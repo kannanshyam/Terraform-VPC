96323fea4827bf9642c0de2bfe9a51b27c8118af
Kannan MS <94454999+kannanshyam@users.noreply.github.com>
2021-12-07 12:06:29 +0530

    Create 01-01-main-instance.tf

---
+++ 01-01-main-instance.tf
@@ -0,0 +1,224 @@
+###############################################################
+#                    Public Key Pair
+###############################################################
+
+resource "aws_key_pair" "key" {
+  key_name      = "${var.project}-key"
+  public_key    = file("id_rsa.pub")
+  
+  tags = {
+    Name = "${var.project}-key"
+  }
+}
+
+###############################################################
+#                  Bastion Security Group
+###############################################################
+
+resource "aws_security_group" "bastion" {
+    
+  vpc_id        = aws_vpc.terra-vpc.id
+  name          = "${var.project}-bastion"
+  description   = "allow 22 port"
+
+  ingress = [
+    {
+      description      = ""
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+    } 
+      
+  ]
+
+  egress = [
+    { 
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "${var.project}-bastion"
+  }
+}
+
+###############################################################
+#                  Webserver Security Group
+###############################################################
+
+resource "aws_security_group" "webserver" {
+    
+  vpc_id      = aws_vpc.terra-vpc.id
+  name        = "${var.project}-webserver"
+  description = "allow 80,443,22 port"
+
+  ingress = [
+    {
+      description      = ""
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+    },
+    {
+      description      = ""
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+    },
+    {
+      description      = ""
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = []
+      ipv6_cidr_blocks = []
+      security_groups  = [ aws_security_group.bastion.id ]
+    }
+      
+  ]
+
+  egress = [
+     { 
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "${var.project}-webserver"
+  }
+}
+
+###############################################################
+#                  Database Security Group
+###############################################################
+
+resource "aws_security_group" "database" {
+    
+  vpc_id      = aws_vpc.terra-vpc.id
+  name        = "${var.project}-database"
+  description = "allow 3306,22 port"
+
+  ingress = [
+    
+    {
+      description      = ""
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = []
+      ipv6_cidr_blocks = []
+      security_groups  = [ aws_security_group.bastion.id ]
+    },
+    {
+      description      = ""
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+      from_port        = 3306
+      to_port          = 3306
+      protocol         = "tcp"
+      cidr_blocks      = []
+      ipv6_cidr_blocks = []
+      security_groups  = [ aws_security_group.webserver.id ]
+    }
+      
+  ]
+
+  egress = [
+     { 
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "${var.project}-database"
+  }
+}
+
+
+###############################################################
+#                  EC2 Instance :: Bastion
+###############################################################
+
+resource "aws_instance" "bastion" {
+
+  ami                          =  var.ami
+  instance_type                =  var.type
+  subnet_id                    =  aws_subnet.public1.id
+  vpc_security_group_ids       =  [ aws_security_group.bastion.id]
+  key_name                     =  aws_key_pair.key.id
+  userdata                     =  file("setup.sh")
+  
+  tags = {
+    Name = "${var.project}-bastion"
+  }
+}
+
+###############################################################
+#                  EC2 Instance :: Webserver
+###############################################################
+
+resource "aws_instance" "webserver" {
+
+  ami                          =  var.ami
+  instance_type                =  var.type
+  subnet_id                    =  aws_subnet.public2.id
+  vpc_security_group_ids       =  [ aws_security_group.webserver.id]
+  key_name                     =  aws_key_pair.key.id
+  
+  tags = {
+    Name = "${var.project}-webserver"
+  }
+}
+
+###############################################################
+#                  EC2 Instance :: Database
+###############################################################
+
+resource "aws_instance" "webserver" {
+
+  ami                          =  var.ami
+  instance_type                =  var.type
+  subnet_id                    =  aws_subnet.public2.id
+  vpc_security_group_ids       =  [ aws_security_group.webserver.id]
+  key_name                     =  aws_key_pair.key.id
+  
+  tags = {
+    Name = "${var.project}-webserver"
+  }
+}
+
+
